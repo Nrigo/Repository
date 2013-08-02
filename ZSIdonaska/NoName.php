a3db2eb58761ca9d2010cc25d01dd4836f59fad7
Braunson <braunson@gmail.com>
2013-08-02 14:48:57 -0400

    Update NoName.php
    Missing ), code correction

--- ZSIdonaska/NoName.php
+++ ZSIdonaska/NoName.php
@@ -2,7 +2,7 @@
 
 include_once 'spojenie.php';
 
-mysql_query("DELETE FROM objednavky WHERE id= '".mysql_real_escape_string($_GET["objednavka_vymazat"]."'");
+mysql_query("DELETE FROM objednavky WHERE id= '".mysql_real_escape_string($_GET["objednavka_vymazat"])."'");
 
 ?>
 
